becee444bdc1288bafa18ddc05bba10a9926f4fa
Вадим Бежков <vadimbezhkov3112@gmail.com>
2020-12-29 22:28:54 +0300

    update

--- HomeworkStruct/Program.cs
+++ HomeworkStruct/Program.cs
@@ -11,34 +11,34 @@ namespace HomeworkStruct
         static void Main(string[] args)
         {
             Product apple = new Product(1,54m,44,"apple");
-            Product apple2 = new Product(2, 56m, 44, "apple2");
-            Product apple3 = new Product(3, 57m, 44, "apple3");
-            Product apple4 = new Product(4, 51m, 44, "apple4");
-            Product apple5 = new Product(5, 53m, 44, "apple5");
-            Product apple6 = new Product(6, 55m, 44, "apple6");
-            Product apple7 = new Product(7, 44m, 44, "apple7");
+            Product orange = new Product(56m, 44, "orange");
+            Product grapefruit = new Product(3, 57m, 44, "grapefruit");
+            Product pineapple = new Product(4, 51m, 44, "pineapple");
+            Product potato = new Product(5, 53m, 44, "potato");
+            Product cucumber = new Product(6, 55m, 44, "cucumber");
+            Product tomato = new Product(7, 44m, 44, "tomato");
 
-            Product bananas = new Product(8,44,"banan");
-            Product bananas2 = new Product(8, 44,55);
-            Product bananas3 = new Product(8, 44,99);
-            Product bananas4 = new Product(8, 44);
+            Product sugar = new Product(8,44m, "sugar");
+            Product noname = new Product(9, 44m,55);
+            Product noname1 = new Product(10, 44m,99);
+            Product noname2 = new Product(11,48m);
 
 
             Struct_Sklad sklad = new Struct_Sklad();
             sklad.Sklad();
 
             sklad.AddProducts(apple);
-            sklad.AddProducts(apple2);
-            sklad.AddProducts(apple3);
-            sklad.AddProducts(apple4);
-            sklad.AddProducts(apple5);
-            sklad.AddProducts(apple6);
-            sklad.AddProducts(apple7);
+            sklad.AddProducts(orange);
+            sklad.AddProducts(grapefruit);
+            sklad.AddProducts(pineapple);
+            sklad.AddProducts(potato);
+            sklad.AddProducts(cucumber);
+            sklad.AddProducts(tomato);
 
-            sklad.AddProducts(bananas);
-            sklad.AddProducts(bananas2);
-            sklad.AddProducts(bananas3);
-            sklad.AddProducts(bananas4);
+            sklad.AddProducts(sugar);
+            sklad.AddProducts(noname);
+            sklad.AddProducts(noname1);
+            sklad.AddProducts(noname2);
 
             sklad.GetProductsInfo();
 
